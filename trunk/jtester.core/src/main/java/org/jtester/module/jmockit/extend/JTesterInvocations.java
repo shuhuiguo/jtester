418a803079cdedddf2ebf48d94614f7b52e8364c
darui.wu <darui.wu@gmail.com>
2013-10-08 01:47:09 +0000

    remove

--- trunk/jtester.core/src/main/java/org/jtester/module/jmockit/extend/JTesterInvocations.java
+++
@@ -1,7 +0,0 @@
-package org.jtester.module.jmockit.extend;
-
-public interface JTesterInvocations {
-	void returnValue(Object value);
-
-	void returnValue(Object firstValue, Object... remainingValues);
-}
